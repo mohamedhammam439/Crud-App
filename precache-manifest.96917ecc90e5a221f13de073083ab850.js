f199791f99cdfc8393858c0365c36e1072b37d72
mohamedhammam439 <mohamedhammam439@gmail.com>
2020-04-03 11:21:21 +0200

    Updates

---
+++ precache-manifest.96917ecc90e5a221f13de073083ab850.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d3b4dd42fc4ece076dc31a7a89e0b8",
+    "url": "/Crud-App/index.html"
+  },
+  {
+    "revision": "6863313fbfca64815140",
+    "url": "/Crud-App/static/css/main.2f00039d.chunk.css"
+  },
+  {
+    "revision": "3ac0e473e645936f8895",
+    "url": "/Crud-App/static/js/2.525920ee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Crud-App/static/js/2.525920ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6863313fbfca64815140",
+    "url": "/Crud-App/static/js/main.b9638417.chunk.js"
+  },
+  {
+    "revision": "0f68d9d320f5810d573f",
+    "url": "/Crud-App/static/js/runtime-main.ec71bf85.js"
+  },
+  {
+    "revision": "31a0178ae7aaff8ee2f0fa0f7de768a4",
+    "url": "/Crud-App/static/media/wp2036927.31a0178a.jpg"
+  }
+]);
